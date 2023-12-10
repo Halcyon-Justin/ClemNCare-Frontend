f9881231868d4caa8439d5f4b6df5881883c62af
Justin <halcyon.justin@gmail.com>
2023-12-10 00:01:40 -0600

    Add Family Data to Display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './components/login/login.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/admin/components/family-management/family-management.component.ts
+++ src/app/modules/admin/components/family-management/family-management.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FamilyService } from 'src/app/services/family-management.service';
 
 @Component({
+  providers: [FamilyService],
   selector: 'app-family-management',
   templateUrl: './family-management.component.html',
   styleUrls: ['./family-management.component.scss']
 })
-export class FamilyManagementComponent {
+export class FamilyManagementComponent implements OnInit {
+  familyData: any;
+
+  constructor(private familyService: FamilyService) { }
+
+  ngOnInit(): void {
+    this.getFamilyData();
+  }
+
+  getFamilyData(): void {
+    this.familyService.getFamilyData().subscribe(
+      (data: any) => {
+        this.familyData = data.data; // Assuming family data is nested under 'data' key
+      },
+      error => {
+        console.error('Error fetching family data:', error);
+      }
+    );
+  }
 
 }
